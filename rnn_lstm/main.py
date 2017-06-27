37d1559578c88dfe71704130163b2501df55f682
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-06-27 12:23:45 +0200

    clearing text

--- rnn_lstm/main.py
+++ rnn_lstm/main.py
@@ -4,9 +4,44 @@ import csv
 import numpy as np
 import data_provider as dp
 import codecs
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
 
 TRAINING_MESSAGE_ON = True
 
+
+def review_to_words( raw_review ):
+    # Function to convert a raw review to a string of words
+    # The input is a single string (a raw movie review), and
+    # the output is a single string (a preprocessed movie review)
+    #
+    # 1. Remove HTML
+    #raw_review = BeautifulSoup(raw_review).get_text()
+    #
+    #1.1 Remove Url
+    GRUBER_URLINTEXT_PAT = r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))'
+
+    raw_review = re.sub(GRUBER_URLINTEXT_PAT, "<url>", raw_review)
+
+    # 2. Remove number
+    raw_review = re.sub("\d+", "<digit>", raw_review)
+    #letters_only = re.sub("[^a-zA-Z]", " ", raw_review)
+    #
+    # 3. Convert to lower case, split into individual words
+    words = raw_review.lower().split()
+    #
+    # 4. In Python, searching a set is much faster than searching
+    #   a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))
+    #
+    # 5. Remove stop words
+    meaningful_words = [w for w in words if not w in stops]
+    #
+    # 6. Join the words back into one string separated by space,
+    # and return the result.
+
+    return( " ".join( meaningful_words ))
 
 def main(train_user, train_data, validation_data, lst_test_data):
     log = open('log.txt', 'a+')
@@ -124,7 +159,7 @@ with open(r'../tumblr_twitter_scrapper/username_pair_filtered.csv', 'r', encodin
             tweet_reader = csv.reader(tweets_file, delimiter=' ')
             for item in tweet_reader:
                 if item[3] == 'True':
-                    lst_tweets.append(item[1])
+                    lst_tweets.append(review_to_words(item[1]))
             if len(lst_tweets) < 100:
                 continue
 
@@ -143,7 +178,7 @@ with open(r'../tumblr_twitter_scrapper/username_pair_filtered.csv', 'r', encodin
                 tumblr_reader = csv.reader(tumblr_file, delimiter=' ')
                 for post in tumblr_reader:
                     if post[5] == 'True':
-                        lst_posts.append(post[4])
+                        lst_posts.append(review_to_words(post[4]))
                 if len(lst_posts) < 25:
                     continue
             lst_posts = np.array(lst_posts)
