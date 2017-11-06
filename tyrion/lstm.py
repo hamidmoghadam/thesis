9da995c55342b323baf74a9b9a1bf3e0a8f799d8
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-06 11:21:34 +0100

    .

--- tyrion/lstm.py
+++ tyrion/lstm.py
@@ -72,7 +72,7 @@ def RNN(x, weights, biases, dropout):
     return tf.matmul(output, weights['out']) + biases['out']
 
 
-filename = '../../glove.twitter.27B/glove.twitter.27B.25d.txt'
+filename = '../../glove.twitter.27B/glove.twitter.27B.'+str(n_hidden)+'d.txt'
 
 
 def loadGloVe(filename):
