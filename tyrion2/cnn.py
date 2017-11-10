df0d0338539ccde2e23dee15f8794d914fa5f43d
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-10 11:21:30 +0100

    .

--- tyrion2/cnn.py
+++ tyrion2/cnn.py
@@ -61,10 +61,10 @@ def conv_net_2(x, n_classes, dropout, is_training):
 
     x = tf.reshape(x, shape=[-1, n_input, n_hidden, 1])
     print(x)
-    conv5 = tf.layers.conv2d(x, 100, (5, 1), activation=tf.nn.relu)
-    conv4 = tf.layers.conv2d(x, 100, (4, 1), activation=tf.nn.relu)
-    conv3 = tf.layers.conv2d(x, 100, (3, 1), activation=tf.nn.relu)
-    conv2 = tf.layers.conv2d(x, 100, (2, 1), activation=tf.nn.relu)
+    conv5 = tf.layers.conv2d(x, 10, (5, 1), activation=tf.nn.relu)
+    conv4 = tf.layers.conv2d(x, 10, (4, 1), activation=tf.nn.relu)
+    conv3 = tf.layers.conv2d(x, 10, (3, 1), activation=tf.nn.relu)
+    conv2 = tf.layers.conv2d(x, 10, (2, 1), activation=tf.nn.relu)
     
     conv5 = tf.layers.max_pooling2d(conv5, strides=1, pool_size=(596, n_hidden))
     conv4 = tf.layers.max_pooling2d(conv4, strides=1, pool_size=(597, n_hidden))
