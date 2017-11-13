b8515f90b3b2063834ebcfca70c5fc4f00099655
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-13 15:33:21 +0100

    .

--- tyrion2/run_msg_per_user.sh
+++ tyrion2/run_msg_per_user.sh
@@ -1,26 +1,9 @@
 echo 70 15 85 3
-python hybrid_classifier.py 10 1000 20 70 15 85 3
-python hybrid_classifier.py 10 1000 20 70 15 85 3
-
-echo 50 10 100 4
-python hybrid_classifier.py 10 1000 20 50 10 100 3
-python hybrid_classifier.py 10 1000 20 50 10 100 3
-
-echo 85 70 85 5
-python hybrid_classifier.py 10 1000 20 50 10 85 5
-python hybrid_classifier.py 10 1000 20 50 10 85 5
-
-echo 85 70 50 3
-python hybrid_classifier.py 10 1000 20 85 70 50 3
-python hybrid_classifier.py 10 1000 20 85 70 50 3
-
-echo 30 7 100 5
-python hybrid_classifier.py 10 1000 20 30 7 100 5
-python hybrid_classifier.py 10 1000 20 30 7 100 5
-
-echo 90 70 90 5
-python hybrid_classifier.py 10 1000 20 90 70 90 5
-python hybrid_classifier.py 10 1000 20 90 70 90 5
+for((i = 5; i=<20; i=i+5))
+do
+    python hybrid_classifier.py 10 1000 20 70 $i 85 3 
+    python hybrid_classifier.py 10 1000 20 70 $i 85 3 
+done
 
 
 
