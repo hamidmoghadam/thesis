055ba5bbd56d1691923555903df29e2a09d303ae
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-13 12:35:30 +0100

    .

--- tyrion2/cnn.py
+++ tyrion2/cnn.py
@@ -175,8 +175,8 @@ pred = conv_net_2(inputs, n_classes, dropout, is_training)
 softmax_pred = tf.nn.softmax(pred)
 # Define loss and optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-#optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
 # Evaluate model
 correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
