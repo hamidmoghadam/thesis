b0173f21877745c4dd8fdc8ab42adde2875881ac
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-13 12:57:53 +0100

    .

--- tyrion2/cnn.py
+++ tyrion2/cnn.py
@@ -63,8 +63,8 @@ def conv_net_2(x, n_classes, dropout, is_training):
 
     x = tf.reshape(x, shape=[-1, n_input, n_embedding, 1])
     print(x)
-    conv5 = tf.layers.conv2d(x, 32, (5, n_embedding), activation=tf.nn.relu)
-    conv4 = tf.layers.conv2d(x, 128, (4, n_embedding), activation=tf.nn.relu)
+    conv5 = tf.layers.conv2d(x, 64, (5, n_embedding), activation=tf.nn.relu)
+    conv4 = tf.layers.conv2d(x, 64, (4, n_embedding), activation=tf.nn.relu)
     conv3 = tf.layers.conv2d(x, 64, (3, n_embedding), activation=tf.nn.relu)
     conv2 = tf.layers.conv2d(x, 64, (2, n_embedding), activation=tf.nn.relu)
 
@@ -75,12 +75,7 @@ def conv_net_2(x, n_classes, dropout, is_training):
     conv3 = tf.layers.max_pooling2d(conv3, strides=1, pool_size=(598, 1))
     conv2 = tf.layers.max_pooling2d(conv2, strides=1, pool_size=(599, 1))
     
-    print(conv4)
-    conv5 = tf.layers.conv2d(x, 1, (1, 32), activation=tf.nn.relu)
-    conv4 = tf.layers.conv2d(x, 1, (1, 128), activation=tf.nn.relu)
-    conv3 = tf.layers.conv2d(x, 1, (1, 64), activation=tf.nn.relu)
-    conv2 = tf.layers.conv2d(x, 1, (1, 64), activation=tf.nn.relu)
-
+    
 
     print(conv4)
     #conv5 = tf.contrib.layers.flatten(conv5)
