f6c278297109a5f0be33225e641c23bd36891237
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-14 09:40:15 +0100

    .

--- tyrion2/run_msg_per_user.sh
+++ tyrion2/run_msg_per_user.sh
@@ -1,14 +1,14 @@
 
-for((i = 40; i<85; i=i+5))
-do
-    echo 15 $i 100 3
-    for((j = 0; j<5; j++))
-    do
-        python rnncnn.py 10 1000 20 15 $i 100 3 0.0005 0.5 
-    done
-done
+#for((i = 40; i<85; i=i+5))
+#do
+#    echo 15 $i 100 3
+#    for((j = 0; j<5; j++))
+#    do
+#        python rnncnn.py 10 1000 20 15 $i 100 3 0.0005 0.5 
+#    done
+#done
 
-for((i = 50; i<120; i=i+5))
+for((i = 85; i<120; i=i+5))
 do
     echo 15 85 $i 3
     for((j = 0; j<5; j++))
