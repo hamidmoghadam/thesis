9733de37bddc90f6fedbd977036fdd74ebb6d02b
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-14 12:18:42 +0100

    .

--- tyrion2/auto_tuner.py
+++ tyrion2/auto_tuner.py
@@ -14,9 +14,9 @@ for test_no in range(30) :
             'letter_h' : str(letter_hidden_layer[np.random.randint(4,size=1)[0]]),
             'dropout' : str(dropout[np.random.randint(5,size=1)[0]]),
             'w_embedding' : str(word_embedding[np.random.randint(5,size=1)[0]]),
-            'l_embdedding' : str(letter_embedding[np.random.randint(5,size=1)[0]]) }
+            'l_embedding' : str(letter_embedding[np.random.randint(5,size=1)[0]]) }
 
-    with open('tune/test_{0}_{1}_{2}_{3}_{4}_{5}.txt'.format(test_no,config['word_h'], config['w_embedding'], config['letter_h'], config['l_embedding'],config['dropout'].repalce('.', '-')), 'w', encoding='utf-8') as f:
+    with open('tune/test_{0}_{1}_{2}_{3}_{4}_{5}.txt'.format(test_no,config['word_h'], config['w_embedding'], config['letter_h'], config['l_embedding'], config['dropout'].repalce('.', '-')), 'w', encoding='utf-8') as f:
         for it in range(30):
             output = subprocess.check_output(['python','rnncnn.py', '10', '1000', '23', config['word_h'], config['w_embedding'], config['letter_h'], config['l_embedding'], '0.0005', config['dropout']])
             f.write(output)
