4086d35cbd56d9205d07ebdb9ece947aed1b5b4c
Hamid Moghaddam <hamid.moghadam85@gmail.com>
2017-11-21 15:20:37 +0330

    result

--- social-media-forensics-master/microblog_authorship_attribution/classification/run_batch.sh
+++ social-media-forensics-master/microblog_authorship_attribution/classification/run_batch.sh
@@ -1,20 +1,9 @@
-max=4000
 
-for((i=50;i<500; i= i+50))
+for((i=1000;i<3500; i= i +500))
 do
-    echo "============================================= number of user 25  number posts is $i ============================================================="
+    echo "============== number of user 10 number posts is $i ==============="
     for((j=0; j<10; j++))
     do
-        python pmsvm_classifier2.py --source-dir-data /Users/hamidmoghaddam/Documents/thesis/social-media-forensics-master/microblog_authorship_attribution/dataset/out_train --output-dir hamid_output --minimal-number-tweets $i  --number-authors 25 --number-tweets $i
-    done
-done
-
-
-for((i=1000;i<$max; i= i +500))
-do
-    echo "============================================= number of user 25 number posts is $i ============================================================="
-    for((j=0; j<10; j++))
-    do
-        python pmsvm_classifier2.py --source-dir-data /Users/hamidmoghaddam/Documents/thesis/social-media-forensics-master/microblog_authorship_attribution/dataset/out_train --output-dir hamid_output --minimal-number-tweets $i  --number-authors 25 --number-tweets $i
+        python pmsvm_classifier2.py --source-dir-data /home/hamidm/Documents/thesis/thesis/social-media-forensics-master/microblog_authorship_attribution/dataset/out_train --output-dir hamid_output --minimal-number-tweets $i  --number-authors 10 --number-tweets $i
     done
 done
