981a292f280dbe981d39a28f0786c1797ff3efdc
hamid moghaddam <hamid.moghadam85@gmail.com>
2017-11-23 19:05:58 +0100

    .:

--- tyrion2/auto_tuner.py
+++ tyrion2/auto_tuner.py
@@ -16,7 +16,7 @@ for test_no in range(100) :
             'w_embedding' : str(word_embedding[np.random.randint(5,size=1)[0]]),
             'l_embedding' : str(letter_embedding[np.random.randint(5,size=1)[0]]) }
 
-    for it in range(10):
+    for it in range(5):
         print('test_{0}_{1}_{2}_{3}_{4}_{5}.txt iteration {6}'.format(test_no,config['word_h'], config['w_embedding'], config['letter_h'], config['l_embedding'], config['dropout'].replace('.', '-'), it))
         with open('tune/test_{0}_{1}_{2}_{3}_{4}_{5}.txt'.format(test_no,config['word_h'], config['w_embedding'], config['letter_h'], config['l_embedding'], config['dropout'].replace('.', '-')), 'a+', encoding='utf-8') as f:
             output = subprocess.check_output(['python','rnncnn.py', '20', '2000', '25', config['word_h'], config['w_embedding'], config['letter_h'], config['l_embedding'], '0.0005', config['dropout']])
