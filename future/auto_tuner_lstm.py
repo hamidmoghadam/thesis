ac5fb484ba5d398b809cb60cec98b1971a286a88
hamid moghaddam <hamid.moghadam85@gmail.com>
2018-01-05 00:40:34 +0330

    .

--- future/auto_tuner_lstm.py
+++ future/auto_tuner_lstm.py
@@ -16,8 +16,8 @@ for test_no in range(100) :
             'w_embedding' : str(word_embedding[np.random.randint(5,size=1)[0]])}
 
     for it in range(5):
-        print('test_{0}_{1}_{2}_{3}_{4}_{5}.txt iteration {6}'.format('20', '2000', '23', test_no,config['word_h'], config['w_embedding'], it))
-        with open('tune_lstm/test_{0}_{1}_{2}_{3}_{4}.txt'.format('20', '2000', '23', test_no,config['word_h'], config['w_embedding']), 'a+', encoding='utf-8') as f:
+        print('test_{0}_{1}_{2}_{3}_{4}_{5}.txt iteration {6}'.format(test_no, '20', '2000', '23',config['word_h'], config['w_embedding'], it))
+        with open('tune_lstm/test_{0}_{1}_{2}_{3}_{4}.txt'.format(test_no,'20', '2000', '23' ,config['word_h'], config['w_embedding']), 'a+', encoding='utf-8') as f:
             output = subprocess.check_output(['python','classifier.py', '20', '2000', '23', config['word_h'], config['w_embedding']])
             f.write(output.decode('utf8'))
             f.write('\n\n\n')
